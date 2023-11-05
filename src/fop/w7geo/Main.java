6be27caa345dd7f4f662c7bc93ff5d43bef9d7e5
LAPTOP-UI91R3FP\areva <arevadzedachi123@gmail.com>
2023-11-06 01:07:52 +0400

    message

--- src/fop/w7geo/Main.java
+++
@@ -1,9 +0,0 @@
-package fop.w7geo;
-
-public class Main {
-    public static void main(String[] args) {
-        BaseArea baseArea = new Rectangle(12,12);
-        Prism prism = new Prism(baseArea, 12);
-        System.out.println(prism.isCube());
-    }
-}
